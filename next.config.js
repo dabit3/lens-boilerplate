b91b0a2b8bf4767484289e27baad492feb23b35e
Nader Dabit <dabit3@gmail.com>
2023-09-13 18:43:23 -0500

    updated next config for static export

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
 
 module.exports = nextConfig
