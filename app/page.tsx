6383689273840129188ed3fd489670edb3f9dff4
Nader Dabit <dabit3@gmail.com>
2023-10-19 08:55:47 -0600

    updated landing page

--- app/page.tsx
+++ app/page.tsx
@@ -50,19 +50,19 @@ export default function Home() {
         </a>
 
         <a
-          href="https://aave.notion.site/Lensverse-a5926eab1d8148eda10053af9f4e28a0"
+          href="https://walletconnect.com/"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
-            Lensverse{' '}
+            WalletConnect{' '}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Lensverse.
+            Configure Web3Modal Project ID
           </p>
         </a>
 
